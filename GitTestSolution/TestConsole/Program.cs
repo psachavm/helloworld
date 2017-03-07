dc8f4c29eb0349fd61aef2097d6437ca8366bfdb
psachavm <paulosacha.vm@gmail.com>
2017-03-06 23:20:40 -0300

    Add files via upload

---
+++ GitTestSolution/TestConsole/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Teste Git !!!");
+
+            ShowMessage("Teste 1");
+
+            Console.ReadLine();
+
+
+        }
+
+        private static void ShowMessage(String msg)
+        {
+            Console.WriteLine("A mensagem é: " + msg);
+        }
+    }
+}
