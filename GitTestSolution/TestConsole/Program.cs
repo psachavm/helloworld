5deb3371a20120a7b4b0f4f85264bedd426dcb1e
Paulo Sacha <paulosacha.vm@gmail.com>
2017-04-01 23:27:38 -0300

    Adição dos métodos Somar e Multiplicar

--- GitTestSolution/TestConsole/Program.cs
+++ GitTestSolution/TestConsole/Program.cs
@@ -12,8 +12,42 @@ namespace TestConsole
         {
             Console.WriteLine("Teste Git !!!");
 
-            ShowMessage("Teste 1");
-            ShowMessage("Teste 2");
+            Console.WriteLine("");
+            Console.WriteLine("Digite a opção desejada: ");
+            Console.WriteLine("  1 - Soma dois numeros ");
+            Console.WriteLine("  2 - Multiplica dois numeros ");
+
+            string opc = Console.ReadLine();
+
+            Console.WriteLine("Digite o valor 1: ");
+            var valor1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor 2: ");
+            var valor2 = int.Parse(Console.ReadLine());
+
+            int resultado = 0;
+
+            switch (opc)
+            {
+
+                case "1":
+                    resultado = Somar(valor1, valor2);
+                    ShowMessage("O resultado é: " + resultado.ToString());
+                    break;
+                case "2":
+                    resultado = Multiplicar(valor1, valor2);
+                    ShowMessage("O resultado é: " + resultado.ToString());
+
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida.");
+                    break;
+
+            }   
+
+            
+            //ShowMessage("Teste 1");
+            //ShowMessage("Teste 2");
+
 
 
             Console.ReadLine();
@@ -24,6 +58,16 @@ namespace TestConsole
         private static void ShowMessage(String msg)
         {
             Console.WriteLine("A mensagem é: " + msg);
+        }
+
+        private static int Multiplicar(int a, int b)
+        {
+            return (a * b);
+        }
+
+        private static int Somar(int a, int b)
+        {
+            return (a + b);
         }
     }
 }
